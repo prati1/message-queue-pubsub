21bf42ce0aaef93dd452231b0bedd0583d016382
pratiksha977 <pratiksha@proshore.eu>
2019-12-05 23:26:02 +0545

    frontend design update

--- subscriber-frontend/public/javascripts/workerservice.js
+++ subscriber-frontend/public/javascripts/workerservice.js
@@ -3,11 +3,13 @@ $(document).ready(() => {
   let socket = io.connect(`http://localhost:8018`);
   // get message from subscriber and display
   socket.on("randomMessage", data => {
-    let newMessage = $(`.messageList`)
+    let newMessage = $(`.message-cont`)
       .first()
       .clone();
-    console.log(data);
-    newMessage.text(data.message);
-    $(`.list-group`).append(newMessage);
+    // console.log(data);
+    newMessage.find(`p`).text(data.message);
+    newMessage.find(`.badge`).text(data.priority);
+    newMessage.find(`.time-right`).text(data.timestamp);
+    $(`#mainContainer`).append(newMessage);
   });
 });
